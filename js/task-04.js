5b8fdc02f0c84a7258fb603de5ee332c68d9366c
Naiks90 <sasha.naiko@gmail.com>
2021-11-16 14:17:40 +0200

    fix task 4

--- js/task-04.js
+++ js/task-04.js
@@ -3,7 +3,9 @@ const pricePerDroid = 3000;
 // let totalPrice = pricePerDroid * countDroid;
 
 const countDroid = prompt('Сколько дроидов Вам нужно');
-if (countDroid <= null) {
+if (countDroid === '' || isNaN(countDroid)) {
+  alert(`'Вы вели не число'`);
+} else if (countDroid <= null) {
   alert(`'Отменено пользователем!'`);
 } else if (true) {
   const totalPrice = pricePerDroid * countDroid;
